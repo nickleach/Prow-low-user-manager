f326fc80199fdb21edd502ec446719d2f7391e0e
Nick Leach <nickleach22@gmail.com>
2015-10-13 00:54:57 -0400

    Set up index.html

--- app/routes/api.js
+++ app/routes/api.js
@@ -16,6 +16,7 @@ apiRouter.post('/authenticate', function(req,res){
   User.findOne({
     username: req.body.username
   }).select('name username password').exec(function(err, user){
+
     if (err) throw err;
 
     //no user with that username was found
