6d7ea138207020f853b01eba02a6b62f6102c9ce
Nick Leach <nickleach22@gmail.com>
2015-12-10 14:31:21 -0500

    commiting

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,7 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 var bcrypt 		 = require('bcrypt-nodejs');
 
-// user schema 
+// user schema
 var UserSchema   = new Schema({
 	name: String,
 	username: { type: String, required: true, index: { unique: true }},
