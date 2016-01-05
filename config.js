da99aee1177fd95b83795ef771fc9db3ecd3642d
Nick Leach <nickleach22@gmail.com>
2016-01-05 11:32:22 -0500

    re added conifg

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'port' : process.env.PORT || 8080,
+  'database' : 'mongodb://nick:nick@ds037155.mongolab.com:37155/prolowtest',
+  'secret': 'ilovescotchscotchyscotchscotch'
+};
