8647fd5e290cd7f01892f57513ca91bce7f34ebd
Nick Leach <nickleach22@gmail.com>
2016-01-05 11:28:49 -0500

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  'port' : process.env.PORT || 8080,
-  'database' : 'mongodb://nick:nick@ds037155.mongolab.com:37155/prolowtest',
-  'secret': 'ilovescotchscotchyscotchscotch'
-};
