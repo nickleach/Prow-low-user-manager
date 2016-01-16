20856f9c590cffe4dc4695201462751a6c64048b
Nick Leach <nickleach22@gmail.com>
2016-01-16 12:48:01 -0500

    fixed config route

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   'database' : 'mongodb://nick:nick@ds037155.mongolab.com:37155/prolowtest',
   'secret': 'ilovescotchscotchyscotchscotch',
   'company' : 'Pro Low',
-  'rootUrl' : 'admin.prolowputting.com',
+  'rootUrl' : 'http://admin.prolowputting.com/',
   'appUrl' : 'http://prolowputting.com/',
   'email': {
     'testimonial' : 'nickleach22@gmail.com',
